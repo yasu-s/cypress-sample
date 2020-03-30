a7904874bb4605012a932c7395ab843d5e52225a
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2020-03-30 12:35:34 +0900

    spec追加

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,35 +1,32 @@
 /// <reference types="cypress" />
 
-describe('Actions', () => {
+describe('index.html', () => {
   beforeEach(() => {
-    cy.visit('https://example.cypress.io/commands/actions')
+    cy.visit('./public/index.html');
   })
 
-  describe('type', () => {
-    it('.type() - type into a DOM element', () => {
-      // https://on.cypress.io/type
-      cy.get('.action-email')
-        .type('fake@email.com').should('have.value', 'fake@email.com')
+  describe('increment', () => {
+    it('ok: one click', () => {
+      // exercise
+      cy.get('[data-testid="increment"]')
+        .click();
 
-        // .type() with special character sequences
-        .type('{leftarrow}{rightarrow}{uparrow}{downarrow}')
-        .type('{del}{selectall}{backspace}')
+      // verify
+      cy.get('[data-testid="content"]')
+        .should('exist')
+        .contains('1');
+    });
 
-        // .type() with key modifiers
-        .type('{alt}{option}') //these are equivalent
-        .type('{ctrl}{control}') //these are equivalent
-        .type('{meta}{command}{cmd}') //these are equivalent
-        .type('{shift}')
+    it('ok: two click', () => {
+      // exercise
+      cy.get('[data-testid="increment"]')
+        .click()
+        .click();
 
-        // Delay each keypress by 0.1 sec
-        .type('slow.typing@email.com', { delay: 100 })
-        .should('have.value', 'slow.typing@email.com')
-
-      cy.get('.action-disabled')
-        // Ignore error checking prior to type
-        // like whether the input is visible or disabled
-        .type('disabled error checking', { force: true })
-        .should('have.value', 'disabled error checking')
-    })
+      // verify
+      cy.get('[data-testid="content"]')
+        .should('exist')
+        .contains('2');
+    });
   });
 })
